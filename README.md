68a6080d95f09a23f4b52a8aa64dc9b51ba085bc
Jay Cheroske <jay@portal.org>
2017-04-06 20:18:11 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# police-daily-activity-importer
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const { resolve } = require('path')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack')
+
+const PROD = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  context: __dirname,
+  devtool: PROD ? 'nosources-source-map' : 'source-map',
+  entry: './src/index.js',
+  externals: {
+//    invariant: 'invariant',
+    lodash: 'lodash',
+//    warning: 'warning'
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'index.js',
+    path: resolve(__dirname, './dist')
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development'
+    }),
+
+    ...(PROD ? [new UglifyJSPlugin({
+      compressor: {
+        unsafe: true
+      }
+    })] : [])
+  ],
+  resolve: {
+    extensions: ['.js'],
+    modules: ['lib', 'node_modules']
+  },
+  target: 'node'
+}
